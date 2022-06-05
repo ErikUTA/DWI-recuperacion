6523dbea04bf0111baea1baae8da28787d336157
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-06-05 10:24:10 -0500

    added filter function to create an array with the elements that meet the condition

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// map - The map function allows us to iterate an array and then modify its elements through a function called callback.
-// use case: increase employee salaries by $1,000
+// filter - The filter function allows us to create a new array with the elements that meet the given condition.
+// use case: show people over 18 years old.
 
-const salary = [12000, 7000, 15000, 20000, 7000, 15000];
-const newSalary = salary.map(e => {
-    console.log(e + 1000);
-});
+const persons = [20, 17, 19, 15];
+const accepted = persons.filter(element => element > 18);
+console.log(accepted);
